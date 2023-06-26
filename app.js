168a3ca6f14b15aba336e10e69c1c3d1e384ea49
DesarrollosFVP <federico.villalba.petrelli@gmail.com>
2023-06-26 16:50:25 -0300

    feat: basic structure

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views', 'home.html')))
 
-app.get('./*', (req, res) => res.sendFile(path.join(__dirname, 'views', '404.html')))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname,'views', '404.html')))
 
 
 app.listen(PORT, ()=> console.log(`Server running in http://localhost:${PORT}`))
