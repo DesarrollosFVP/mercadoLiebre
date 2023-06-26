94eac0f4a96c2e2d5dfe0f86298f1d073c45b4bb
DesarrollosFVP <federico.villalba.petrelli@gmail.com>
2023-06-26 12:04:09 -0300

    feat: add project 'MercadoLiebre'

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require ('path')
+
+const express = require ('express')
+const app = express()
+
+const PORT = 3030
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views', 'home.html')))
+
+app.get('./*', (req, res) => res.sendFile(path.join(__dirname, 'views', '404.html')))
+
+
+app.listen(PORT, ()=> console.log(`Server running in http://localhost:${PORT}`))
+
